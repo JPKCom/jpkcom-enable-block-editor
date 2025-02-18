0db93bd38e7aab7ae835e67a80d5925e9cdc1b82
Jean Pierre Kolb <jp.kolb@creationell.de>
2025-02-18 17:10:33 +0100

    v1.0.2

--- jpkcom-enable-block-editor.php
+++ jpkcom-enable-block-editor.php
@@ -1,11 +1,16 @@
 <?php
 /*
 Plugin Name: JPKCom Enable Block Editor
+Plugin URI: https://www.jpkc.com/
 Description: Enables the Block Editor for Admin (User ID:1).
-Version: 1.0.1
+Version: 1.0.2
 Author: Jean Pierre Kolb <jpk@jpkc.com>
 Author URI: https://www.jpkc.com
 Requires Plugins: blockstudio
+Requires at least: 6.7
+Requires PHP: 8.3
+License: MIT
+License URI: https://opensource.org/license/MIT
 GitHub Plugin URI: JPKCom/jpkcom-enable-block-editor
 Primary Branch: main
 */
